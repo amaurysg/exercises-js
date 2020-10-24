ea1342d302c5ca1c9a49f90ed1014d66606f1eeb
Amaury Soto <amaurysotog@gmail.com>
2020-10-24 13:12:19 -0500

    Excercises 1-2

---
+++ one.js
@@ -0,0 +1,23 @@
+
+//EJERCICIO 1
+/* var num1 = parseInt(prompt("Primer Número"))
+var num2 = parseInt(prompt("Segundo Número"))
+
+function sumar(num1, num2) {
+  resultado = num1 + num2
+  parseInt(resultado)
+}
+sumar(num1, num2)
+alert(`La suma total fue: ${resultado}`) */
+
+
+//EJERCICIO 2 
+/* var temp = parseInt(prompt("Dame una temperatura"))
+
+function conversor(temp) {
+
+  resultado = Math.round((temp * (9 / 5) + 32))
+
+}
+conversor(temp)
+alert(`${temp}°C = ${resultado}°F`) */
