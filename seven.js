0c646a3568c2c17833223ed4987c81da3fdd2ca6
Amaury Soto <amaurysotog@gmail.com>
2020-10-29 22:20:05 -0500

    Exercises until 46

---
+++ seven.js
@@ -0,0 +1,148 @@
+//EJERCICIO 41
+/* var data1 = prompt("Ingrese dato")
+var data2 = prompt("Ingrese dato")
+var data3 = prompt("Ingrese dato")
+var array = []
+const addData = (data1, data2, data3) => {
+  array.unshift(data1, data2, data3)
+}
+addData(data1, data2, data3)
+console.log(array) */
+/* var data2= prompt("Ingrese dato")
+var data3= prompt("Ingrese dato") */
+
+//EJERCICIO 42
+/* var user = "1,2,3"
+ var array = user.split(",")
+console.log(array) */
+/* for (i = 0; i < user.length; i++) {
+  user.charAt(i)
+  console.log(user.charAt(i))
+} */
+
+//EJERCICIO 43 
+/* var array = ["Hola", "Mundo"]
+var arrayToStr = array.join(" ")
+console.log(arrayToStr) */
+
+//UNION, INTERSECCION Y CONTEO 
+
+//EJERCICIO 44
+/* var array1 = ["Amaury", "Hola", "Mundo", "ok"]
+var array2 = ["Julio", "Hello", "Word", "right"] */
+
+
+/* var set1 = new Set([1, 2, 3, 4]),
+  set2 = new Set([3, 4, 5, 6]);
+
+const intersection = new Set(Array.from(set1).filter(x => set2.has(x)));//Set {3, 4}
+const difference = new Set(Array.from(set1).filter(x => !set2.has(x))); //Set {1, 2}
+
+console.log(intersection)
+console.log(difference) */
+
+/* var a = [1, 5, 3, 4],
+  b = [1, 4]
+
+var result = a.filter(function (e) {
+  return b.indexOf(e) == -1
+});
+
+console.log(result); */
+/* var arr1 = ["1", "2", "3", "6", "8", "1"]
+console.log("Array1:", arr1)
+var arr2 = ["1", "2", "4"]
+console.log("Array2:", arr2)
+var arrInt = []
+
+for (i = 0; i < arr1.length; i++) {
+  for (j = 0; j < arr2.length; j++) {
+    if (arr1[i] === arr2[j]) {
+      arrInt.push(arr1[i])
+    }
+  }
+}
+console.log("Array Intersection:", arrInt) */
+
+//EJERCICIO 45
+/* var arr1 = ["1", "2", "3"]
+console.log("Array1:", arr1)
+var arr2 = ["1", "2", "4"]
+console.log("Array2:", arr2)
+var arrUnion = arr1.concat(arr2).sort()
+console.log("Array Union:", arrUnion) */
+
+/* var array1 = [
+  { "nombre": "paco", "edad": "21" }
+];
+
+// Array con 2 objetos
+var array2 = [
+  { "nombre": "matias", "edad": "25" },
+  { "nombre": "juan", "edad": "31" },
+];
+
+array1 = array1.concat(array2);
+console.log(array1); */
+
+
+/* var arr1 = ["1", "2", "3"]
+console.log("Array1:", arr1)
+var arr2 = ["1", "2", "4"]
+console.log("Array2:", arr2)
+var arrUnion = [...arr1, ...arr2]
+
+console.log(arrUnion.sort()) */
+
+
+/* var arr1 = ["1", "2", "3"]
+
+var arr2 = ["1", "2", "4"]
+
+
+var arr3 = []
+
+function arrayUnion() {
+
+  for (i = 0; i < arr1.length; i++) {
+    arr3.push(arr1[i])
+  }
+  for (j = 0; j < arr1.length; j++) {
+    arr3.push(arr2[j])
+  }
+}
+arrayUnion()
+
+console.log(arr3)
+ */
+
+//Ejercicio 46
+/* var arr1 = [1, 2, 3]
+var arr2 = [4, 5, 6]
+var arrP = []
+
+function compare(arr1, arr2) {
+
+  if (arr1.length == arr2.length) {
+    console.log(`Ambos arrays tienen ${arr1.length} elementos`)
+
+
+  } else {
+    console.log(`No tienen la misma cantidad elementos`)
+  }
+}
+console.log(arrP)
+compare(arr1, arr2) */
+
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var r = [];
+
+for (i = 0; i < arr1.length; i++) {
+  r[i] = arr1[i] + arr2[i];
+}
+
+console.log(r);
+
+
--- six.js
+++ six.js
@@ -38,15 +38,67 @@ const newArray = array.map((e) => {
 console.log(newArray) */
 
 //EJERCICIO 35
-var array = [1, 2, 3, 4, 5, 25, 6, 1, 100]
-/* 
+/* var array = [1, 2, 3, 4, 5, 25, 6, 1, 100] */
+/*
 const sum = array.reduce((acc, value) => acc + value)
 const prom = sum / array.length
 console.log(prom) */
-cont = 0
+/* cont = 0
 for (i = 0; i < array.length; i++) {
   cont += array[i]
 }
-console.log("Sumatoria", cont, `promedio con ${array.length} =`)
+console.log("Sumatoria", cont, `promedio con ${array.length} = `)
 var prom = Math.round(cont / array.length)
-console.log(prom)
+console.log(prom) */
+
+
+//EJERCICIO 36
+/* var array = []
+function getRandomArbitrary(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+for (i = 0; i < 20; i++) {
+  array.push(getRandomArbitrary(1, 20))
+}
+console.log(array) */
+
+//EJERCICIO 37
+/* var array = []
+var lim = prompt("Dame un número")
+function getRandomArbitrary(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+for (i = 0; i < lim; i++) {
+  array.push(getRandomArbitrary(1, 20))
+}
+console.log(array)
+console.log(array.sort((a, b) => a - b))
+
+var repetidos = {};
+
+array.forEach(function (numero) {
+  repetidos[numero] = (repetidos[numero] || 0) + 1;
+});
+
+console.log(repetidos);
+
+
+
+let unicos = new Set(array);
+
+console.log("Unicos: ", [...unicos]); */
+
+//EJERCICIO 40
+
+/* var colores = ["azul", "amarillo", "rojo", "verde", "café", "rosa"]
+
+function containColors(color) {
+  if (colores.includes(color)) {
+    console.log("Existe")
+  } else {
+    console.log("NOOO")
+  }
+}
+containColors("verde") */
