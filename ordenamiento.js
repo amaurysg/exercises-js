0db622ce08cd8c5041a79e98c80761f3131c06f4
Amaury Soto <amaurysotog@gmail.com>
2020-11-03 11:41:42 -0500

    Concat flat,  div slice

--- ordenamiento.js
+++ ordenamiento.js
@@ -1,4 +1,4 @@
-//ORDENAMIENTO 
+//ORDENAMIENTO
 //EJERCICIO 48
 
 /* var arr = [1, 2, 3] //[2,1,3]
@@ -34,7 +34,7 @@ var newArr = arr.sort((a, b) => a - b)
 console.log(newArr) */
 
 
-function orden() {
+/* function orden() {
   var arr = [4, 11, 12, 10, 21, 1, 2, 7, 21, 6]
   var aux
   console.log(arr)
@@ -50,7 +50,7 @@ function orden() {
   }
   console.log(arr)
 }
-orden()
+orden() */
 /*
 
 function Burbuja() {
@@ -72,3 +72,65 @@ function Burbuja() {
   console.log(lista); // Mostramos, por consola, la lista ya ordenada
 }
 Burbuja() */
+/*
+var arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+var newArr = arr.flat()
+console.log(newArr)
+ */
+/* var nRepeticiones = 8
+
+for (i = 1; i < nRepeticiones; i++) {
+  console.log(`Hola ${i} mundo`)
+} */
+
+/* function processData(input) {
+  var i;
+  for (i = 0; i < parseInt(input); i++) {
+    console.log("hello world");
+  }
+}
+processData(5) */
+
+/* let arregloOriginal = ["PHP", "JavaScript", "Go", "Rust", "Java", "C", "CSS", "Python"],
+  arregloDeArreglos = []; // Aquí almacenamos los nuevos arreglos
+console.log("Arreglo original: ", arregloOriginal);
+const LONGITUD_PEDAZOS = 3; // Partir en arreglo de 3
+for (let i = 0; i < arregloOriginal.length; i += LONGITUD_PEDAZOS) {
+  let pedazo = arregloOriginal.slice(i, i + LONGITUD_PEDAZOS);
+  arregloDeArreglos.push(pedazo);
+}
+console.log("Arreglo de arreglos: ", arregloDeArreglos); */
+
+/* let alumnos = ["Carlos", "Clara", "Cristina", "Fernando", "Ivan", "Lara", "Mercedes", "Rafael", "Roberto", "Sandra", "Tania", "Raulito", "Milagros"];
+
+let mitad = Math.floor(alumnos.length / 2);
+
+let inicio = alumnos.slice(0, mitad);
+let final = alumnos.slice(mitad); // si no se indica el índice final, se usa la longitud del array como referencia
+
+console.log('Primera parte:', inicio)
+console.log('Segunda parte', final)
+console.log('Array original', alumnos.toString()); */
+
+
+
+/* var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function divArr(arr) {
+  var mitad = Math.floor(arr.length / 2)
+  var newArr = []
+  newArr.push(arr.slice(0, mitad), arr.slice(mitad))
+
+  console.log(newArr)
+}
+divArr(arr) */
+
+var arr = [1, 2, [3, 4]];
+var newArr = arr.flat()
+console.log(newArr)
+
+var arr3 = [1, 2, [3, 4, [5, 6]]];
+console.log("Arr3", arr3)
+
+var newArr3 = arr3.flat(2)
+console.log("new", newArr3)
