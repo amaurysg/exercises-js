c490bc817de748a8ff9ca6e480141efb4b4ceff1
Amaury Soto <amaurysotog@gmail.com>
2020-11-04 15:05:25 -0500

    function found points

--- ordenamiento.js
+++ ordenamiento.js
@@ -124,7 +124,7 @@ function divArr(arr) {
   console.log(newArr)
 }
 divArr(arr) */
-
+/*
 var arr = [1, 2, [3, 4]];
 var newArr = arr.flat()
 console.log(newArr)
@@ -133,4 +133,102 @@ var arr3 = [1, 2, [3, 4, [5, 6]]];
 console.log("Arr3", arr3)
 
 var newArr3 = arr3.flat(2)
-console.log("new", newArr3)
+console.log("new", newArr3) */
+
+
+/* var other = Math.max(...arr)
+console.log(other) */
+
+/* var arr = [1, 2, 3]
+function findLargest(arr) {
+
+  var other = Math.max(...arr)
+
+  console.log(other)
+}
+findLargest(arr) */
+
+
+/* function calculateClosestTo0(arr) {
+  let closest = 0
+  for (let i = 0; i < tempArray.length; i++) {
+    if (closest === 0) {
+      closest = arr[i]
+    } else if (arr[i] > 0 && arr[i] <= Math.abs(closest)) {
+      closest = arr[i]
+    } else if (arr[i] < 0 && -arr[i] < Math.abs(closest)) {
+      closest = arr[i]
+    }
+  }
+  return closest
+}
+tempArray = [-9, 8, 2, -5, 7]
+console.log(calculateClosestTo0(tempArray)) */
+
+
+/* const sequencesConverge = (a, b) => {
+  const [a3, a9, b3, b9] = [a % 3, a % 9, b % 3, b % 9];
+  return (a9 === 0 && b9 === 0) || (a9 * b9 !== 0 && (a3 === b3 || a3 + b3 === 3));
+}
+console.log(sequencesConverge(57, 78));
+console.log(sequencesConverge(471, 480)); */
+
+/* function computeJoinPoint(s1, s2) {
+  console.log(s1, s2);
+  const digitSum = num => {
+    return String(num).split('').reduce((a, b) => parseInt(a) + parseInt(b), 0);
+  }
+  let maxIterations = 1000; // so the browser won't hang if you try something weird
+  while (true) {
+    if (!maxIterations--) {
+      throw new Error('giving up');
+    }
+    if (s1 < s2) {
+      s1 += digitSum(s1);
+      console.log(s1, s2);
+    } else if (s1 > s2) {
+      s2 += digitSum(s2);
+      console.log(s1, s2);
+    } else {
+      return s1;
+    }
+  }
+}
+console.log('join point:', computeJoinPoint(57, 78)); */
+
+
+
+//console.log(sumDigits(18))
+
+
+
+function foundPoints(num1, num2) {
+
+  const sumDigits = (num) => {
+    var res = 0
+    var other = num.toString().split("")
+
+    for (let i = 0; i < other.length; i++) {
+      res += parseInt(other[i])
+    }
+    return res
+  }
+  let maxIterations = 1000
+  while (true) {
+    if (!maxIterations--) {
+      throw new Error("Much iterations 🙄")
+    }
+    if (num1 < num2) {
+      num1 += sumDigits(num1)
+      console.log('num1,num2', num1, num2)
+    } else if (num1 > num2) {
+      num2 += sumDigits(num2)
+      console.log('num1,num2', num1, num2)
+
+    } else {
+      return `${num1} 🤩`
+    }
+  }
+}
+
+console.log(foundPoints(78, 480))
